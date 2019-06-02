21bcc1968c3e4a8d5895367b11ab79ff276dda11
Alvin <jenersigua@Jeners-MacBook-Air.local>
2019-06-02 15:44:16 +0800

    add .env

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const PORT = 3000;
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 app_1.default.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
 });
--- lib/server.ts
+++ lib/server.ts
@@ -1,5 +1,8 @@
 import app from "./app";
-const PORT = 3000;
+require('dotenv').config();
+
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
